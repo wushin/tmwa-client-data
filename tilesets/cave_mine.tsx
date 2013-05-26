2ea336778b4ba30ae5ac36daa8e68be9969b6327
wushin <pasekei@gmail.com>
2013-05-25 21:24:53 -0500

    New Cave Mine set

---
+++ tilesets/cave_mine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cave-mine" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/cave_mine.png" width="512" height="512"/>
+</tileset>
