7a42a31c20941c8c939769b27cf7b3ed6fa641af
wushin <pasekei@gmail.com>
2013-07-24 09:20:47 -0500

    Wip #1

---
+++ tilesets/Tulimshar_Docks2_x2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tulimshar_Docks2_x2" tilewidth="32" tileheight="64">
+ <image source="../graphics/tiles/Tulimshar_Docks_x2.png" width="512" height="320"/>
+</tileset>
---
+++ tilesets/equipment.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="equipment" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/equipment.png" width="480" height="416"/>
+</tileset>
