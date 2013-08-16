ff68a9089cd063fa4e99f2b78e832942756f0e5d
wushin <pasekei@gmail.com>
2013-08-16 11:29:49 -0500

    tilesets and map shuffling preparing dev work

---
+++ tilesets/Crypt-skull_door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Crypt-skull_door" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/Crypt-skull_door.png" width="512" height="128"/>
+</tileset>
---
+++ tilesets/Crypt_reaper_room.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Crypt_reaper_room" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/Crypt_reaper_room.png" width="512" height="352"/>
+</tileset>
