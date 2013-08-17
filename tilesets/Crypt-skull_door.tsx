357eeef665f363cdbeba5a925e20e80e24a34895
wushin <pasekei@gmail.com>
2013-08-17 10:03:17 -0500

    various fixes

--- tilesets/Crypt-skull_door.tsx
+++ tilesets/Crypt-skull_door.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Crypt-skull_door" tilewidth="32" tileheight="32">
+<tileset name="Crypt-skull_door" tilewidth="32" tileheight="128">
  <image source="../graphics/tiles/Crypt-skull_door.png" width="512" height="128"/>
 </tileset>
