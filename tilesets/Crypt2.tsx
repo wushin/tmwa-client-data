f7e938904ded32ff8de835668b3044264ec42408
wushin <pasekei@gmail.com>
2013-09-12 01:31:23 -0500

    More art more art

--- tilesets/Crypt2.tsx
+++ tilesets/Crypt2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Crypt2" tilewidth="32" tileheight="32">
- <image source="../graphics/tiles/Crypt2.png" width="512" height="320"/>
+ <image source="../graphics/tiles/Crypt2.png" width="512" height="448"/>
 </tileset>
