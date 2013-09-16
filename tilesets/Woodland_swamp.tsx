134b069af93704bce6fd082316cbc779601fef48
wushin <pasekei@gmail.com>
2013-09-16 14:51:37 -0500

    Maps Round one

--- tilesets/Woodland_swamp.tsx
+++ tilesets/Woodland_swamp.tsx
@@ -1,4 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Woodland_swamp" tilewidth="32" tileheight="32">
  <image source="../graphics/tiles/Woodland_swamp.png" width="512" height="512"/>
+ <tile id="69">
+  <properties>
+   <property name="animation-delay0" value="32"/>
+   <property name="animation-delay1" value="37"/>
+   <property name="animation-delay2" value="33"/>
+   <property name="animation-delay3" value="38"/>
+   <property name="animation-delay4" value="40"/>
+   <property name="animation-frame0" value="112"/>
+   <property name="animation-frame1" value="113"/>
+   <property name="animation-frame2" value="114"/>
+   <property name="animation-frame3" value="115"/>
+   <property name="animation-frame4" value="116"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="animation-delay0" value="40"/>
+   <property name="animation-delay1" value="42"/>
+   <property name="animation-delay2" value="45"/>
+   <property name="animation-delay3" value="42"/>
+   <property name="animation-delay4" value="40"/>
+   <property name="animation-frame0" value="128"/>
+   <property name="animation-frame1" value="129"/>
+   <property name="animation-frame2" value="130"/>
+   <property name="animation-frame3" value="131"/>
+   <property name="animation-frame4" value="132"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="animation-delay0" value="25"/>
+   <property name="animation-delay1" value="27"/>
+   <property name="animation-delay2" value="24"/>
+   <property name="animation-delay3" value="27"/>
+   <property name="animation-delay4" value="28"/>
+   <property name="animation-frame0" value="96"/>
+   <property name="animation-frame1" value="97"/>
+   <property name="animation-frame2" value="98"/>
+   <property name="animation-frame3" value="99"/>
+   <property name="animation-frame4" value="100"/>
+  </properties>
+ </tile>
 </tileset>
