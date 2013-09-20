fea8626b9eab800525a662f6aa510113cd345d05
wushin <pasekei@gmail.com>
2013-09-20 06:15:16 -0500

    most recent client changes

--- tilesets/Woodland_swamp.tsx
+++ tilesets/Woodland_swamp.tsx
@@ -15,6 +15,38 @@
    <property name="animation-frame4" value="116"/>
   </properties>
  </tile>
+ <tile id="71">
+  <properties>
+   <property name="animation-delay0" value="1200"/>
+   <property name="animation-delay1" value="3"/>
+   <property name="animation-delay10" value="3"/>
+   <property name="animation-delay11" value="3"/>
+   <property name="animation-delay12" value="3"/>
+   <property name="animation-delay13" value="3"/>
+   <property name="animation-delay2" value="3"/>
+   <property name="animation-delay3" value="3"/>
+   <property name="animation-delay4" value="3"/>
+   <property name="animation-delay5" value="3"/>
+   <property name="animation-delay6" value="3"/>
+   <property name="animation-delay7" value="3"/>
+   <property name="animation-delay8" value="3"/>
+   <property name="animation-delay9" value="3"/>
+   <property name="animation-frame0" value="144"/>
+   <property name="animation-frame1" value="145"/>
+   <property name="animation-frame10" value="148"/>
+   <property name="animation-frame11" value="146"/>
+   <property name="animation-frame12" value="147"/>
+   <property name="animation-frame13" value="148"/>
+   <property name="animation-frame2" value="146"/>
+   <property name="animation-frame3" value="147"/>
+   <property name="animation-frame4" value="148"/>
+   <property name="animation-frame5" value="147"/>
+   <property name="animation-frame6" value="146"/>
+   <property name="animation-frame7" value="147"/>
+   <property name="animation-frame8" value="146"/>
+   <property name="animation-frame9" value="147"/>
+  </properties>
+ </tile>
  <tile id="85">
   <properties>
    <property name="animation-delay0" value="40"/>
