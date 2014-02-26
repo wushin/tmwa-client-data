e844abafa17d619884f63e3d4324fdab3bc053cd
wushin <pasekei@gmail.com>
2014-02-26 08:38:31 -0600

    Removing old tilesets

--- tilesets/cave2_sewer_mine_x3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave2_sewer_mine_x3" tilewidth="32" tileheight="96">
- <image source="../graphics/tiles/cave2_sewer_mine_x3.png" width="256" height="96"/>
-</tileset>
--- tilesets/cave2_x2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cave2_x2" tilewidth="32" tileheight="64">
- <image source="../graphics/tiles/cave2_x2.png" width="512" height="128"/>
-</tileset>
