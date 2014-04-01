c7f8ea9b40555ae7add4106d5d10558d1830ea86
wushin <pasekei@gmail.com>
2014-04-01 12:05:51 -0500

    placeholder

--- tilesets/tulimshar1_indoors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tulimshar1_indoors" tilewidth="32" tileheight="32">
- <image source="../graphics/tiles/tulimshar1_indoors.png" width="480" height="320"/>
-</tileset>
