5e4b3ee3be97f4f26079e8bc81fbbe1017f79de3
wushin <pasekei@gmail.com>
2014-04-02 08:27:49 -0500

    fixed non-exported tilesets

---
+++ tilesets/desert_castle_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="desert-castle-2" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/desert_castle_2.png" width="512" height="512"/>
+</tileset>
