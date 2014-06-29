5351c266a609a5da38ab6da6bfcbedbf2d55f342
wushin <pasekei@gmail.com>
2014-06-28 23:03:39 -0500

    Fixes Fringe Tilesets

---
+++ tilesets/woodland_village_x4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="woodland_village_x4" tilewidth="32" tileheight="128">
+ <image source="../graphics/tiles/woodland_village_x4.png" width="128" height="128"/>
+</tileset>
