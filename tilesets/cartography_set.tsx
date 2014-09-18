49872889966a0762781e49ff59c57ad892c393dd
wushin <pasekei@gmail.com>
2014-09-17 20:35:44 -0500

    Map Alpha One

---
+++ tilesets/cartography_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cartography_set" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/cartography_set.png" width="256" height="256"/>
+</tileset>
